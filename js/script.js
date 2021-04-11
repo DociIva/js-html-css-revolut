6f79563116548145ab0f92f8abb98ac003592816
DociIva <doci.iva@hotmail.com>
2021-04-12 00:55:44 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-console.log('Ciao mondo');
-console.log('jQuery', jQuery);
+//console.log('Ciao mondo');
+//console.log('jQuery', jQuery);
+$(document).ready(function(){
+// dropdown-menu 
+
+// referenze
+var dropDown = $('.with-dropdown');
+var dropLinks = dropDown.children('a');
+var dropMenu = dropDown.children('.dropdown-menu');
+
+// clicca e mostra il menu
+dropLinks.click(function(e){
+
+    $(this).next('.dropdown-menu').toggle();
+});
+
+// chiusura doc ready
+});

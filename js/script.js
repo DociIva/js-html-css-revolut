543efbec36e2fa66c1f5f403b25222d689e47837
DociIva <doci.iva@hotmail.com>
2021-04-12 08:50:39 +0200

    mod

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 //console.log('Ciao mondo');
 //console.log('jQuery', jQuery);
 $(document).ready(function(){
-// dropdown-menu 
+  // dropdown-menu 
 
-// referenze
-var dropDown = $('.with-dropdown');
-var dropLinks = dropDown.children('a');
-var dropMenu = dropDown.children('.dropdown-menu');
+   // referenze
+  var dropDown = $('.with-dropdown');
+  var dropLinks = dropDown.children('a');
+  console.log(dropLinks);
+  var listNasc = dropDown.children('.dropdown-menu');
+  console.log(listNasc);
 
-// clicca e mostra il menu
-dropLinks.click(function(e){
-
+  // click
+  dropLinks.click(function(e){
     $(this).next('.dropdown-menu').toggle();
-});
+
+    var menuClick = $(this).next('.dropdown-menu');
+    listNasc.not(menuClick).hide();
+
+    menuClick.toggle();
+    
+  });
+  
+  
+
 
 // chiusura doc ready
 });
